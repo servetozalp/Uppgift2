818710175e983c2c823599e75d6cfc6e72a57ac7
Servet Özalp <127829171+servetozalp@users.noreply.github.com>
2023-04-27 08:45:25 +0000

    lagt js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Nuvarande år
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+// Mobile navigation
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+  
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+  
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+  
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  }
+  checkFlexGap();
